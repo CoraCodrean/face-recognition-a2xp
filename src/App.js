cdf38d8af133eb21f09cd96a9d8afca8b5bd4801
CoraCodrean <50665097+CoraCodrean@users.noreply.github.com>
2019-05-15 22:45:39 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ class App extends Component {
 
 	onButtonClick = () => {
 		this.setState({imageUrl : this.state.input});
-		fetch('http://localhost:3000/imageurl', {
+		fetch('https://intense-brushlands-30025.herokuapp.com/imageurl', {
  			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
@@ -110,7 +110,7 @@ class App extends Component {
 		.then( response => response.json())
 		.then( response => {
 	      if (response) {
-	     	fetch('http://localhost:3000/image', {
+	     	fetch('https://intense-brushlands-30025.herokuapp.com/image', {
 	 			method: 'put',
 				headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify({
